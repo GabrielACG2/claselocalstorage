e9e3c9f58bed5632add41c9f83e8a55ab1ba495e
GabrielACG2 <gabrielalejandrogonzalez2003@gmail.com>
2023-06-01 14:00:28 -0400

    update
    se logro que se muestre una tabla y que muestre una alerta con las variables ingresadas en el  formulario

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var registrar = () =>{
+    let econtenedorTabla= document.getElementById("contenedorTabla")
     let eNombre = document.getElementById("nombre");
     let eApellido = document.getElementById("apellido");
     let nombre = eNombre.value;
@@ -18,7 +19,31 @@ var registrar = () =>{
     console.log(listadoAntiguo)
     console.log(listadoNuevo)
     localStorage.setItem('personas',JSON.stringify(listadoNuevo));
-}
+    //econtenedorTabla.innerHTML=""+listadoNuevo.length;
+    render ="<table>"
+    render+="<tr><th>nombre</th><th>apellido</th><th>accion</tr>"
+
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element = listadoNuevo[i];
+        render+="<tr>"
+        render+="<td>"+element.nombre+"</td>"
+        render+="<td>"+element.apellido+"</td>"
+        render+="<td>"
+        render+="<button id='btneditar"+i+"'>editar</button>"
+        render+="<button id='btneliminar"+i+"'>eliminar</button>"
+        render+="</td>"
+        render+="</tr>"
+        
+    }
+    render+="</table>"
+    econtenedorTabla.innerHTML = render;
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        var eBTN = document.getElementById("btneditar"+i);
+        let element= listadoNuevo[i]
+        eBTN.addEventListener("click",()=>{alert("hola"+element.nombre+""+element.apellido)})
+    }
+    }
+
 
 document.getElementById("btn").addEventListener("click",registrar)
 
